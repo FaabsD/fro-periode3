3540995d23ef655c00205c54738baa63c98ad534
Fabian <19959@ma-web.nl>
2020-04-01 20:17:21 +0200

    Create lab04.js

---
+++ lab04/lab04.js
@@ -0,0 +1,16 @@
+// Pas het script aan volgens de richtlijnen
+let palet = new Array('wit','blauw','groen','rood','zwart');
+document.write('<br />palet is nu: ' + palet);
+
+let nieuwekleur = prompt('Typ een nieuwe kleur in');
+
+// Stop de nieuwekleur ACHTERAAN in de palet array
+document.write('<br />palet is nu: ' + palet);
+
+// Stop de nieuwekleur VOORAAN in de palet array
+let nieuwekleurAchter = prompt( 'Typ een nieuwe kleur in:');
+
+document.write('<br />palet is nu: ' + palet);
+// Sorteer de array
+
+document.write('<br />palet is nu: ' + palet);
