92a3ce66a04da78f097edd4dab62e6e12a935b31
Fabian <19959@ma-web.nl>
2020-04-14 11:41:18 +0200

    aantal fouten eruit gehaald

--- week9/api.js
+++ week9/api.js
@@ -5,18 +5,18 @@
 //          bekend zijn.
 
 let results = [];
-let item, coordinates, address, = {};
+let item, coordinates, address = [];
 
 function users(items) {
     for (let count = 0; count < items.length; count++) {
 
-        item = new Name(items[count].name)
-        coordinates = new Coordinates(items[count].location.coordinates)
-        address = new Address(items[count].location)
+        item = new Name(items[count].name);
+        coordinates = new Coordinates(items[count].location.coordinates);
+        address = new Address(items[count].location);
     }
-    console.table(item)
-    console.table(coordinates)
-    console.table(address.showStreet())
+    console.table(item);
+    console.table(coordinates);
+    console.table(address.showStreet());
 
     // TODO:    Zorg ervoor dat de volgende statements resultaat gaan opleveren
     //          Hiervoor moet je de functie showAddress() afmaken en de constructors voor de
@@ -31,13 +31,13 @@ function users(items) {
 
 function createGetRequest(url, callBack) {
 
-    var request = new XMLHttpRequest()
+    let request = new XMLHttpRequest();
 
     request.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
 
-            let response = JSON.parse(this.response)
-            callBack(response)
+            let response = JSON.parse(this.response);
+            callBack(response);
 
         }
     }
@@ -46,17 +46,17 @@ function createGetRequest(url, callBack) {
         'GET',
         url,
         true
-    )
+    );
 
-    request.send()
+    request.send();
 }
 
 function randomUser(json) {
-    results = json.results.slice()
-    users(results)
+    results = json.results.slice();
+    users(results);
 }
 
-var url = 'https://randomuser.me/api/?results=1'
+const url = 'https://randomuser.me/api/?results=1';
 createGetRequest(url, randomUser);
 // TODO:    check de xhr tab en waarschuwingen tab
 //          De xhr tab heeft subtabs, check deze allemaal
